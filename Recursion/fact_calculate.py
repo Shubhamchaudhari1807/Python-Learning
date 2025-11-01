d111384cf28659d9d1f38b1754d9798fc3fa3653
Shubhamchaudhari1807 <131322489+Shubhamchaudhari1807@users.noreply.github.com>
2025-11-02 00:09:07 +0530

    Recursion Finished

---
+++ Recursion/fact_calculate.py
@@ -0,0 +1,19 @@
+# Fibonacci Calculation
+def factorial(n):
+    if n == 0:  # Base case
+        return 1
+    else:       # Recursive case
+        return n * factorial(n - 1)
+
+print(factorial(5))
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(10))
